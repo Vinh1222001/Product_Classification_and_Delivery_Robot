b613f435e34485ff1aac21a552001ca92b4b45a8
Nguyễn Tuấn Vinh <nguyentuanvinh1222@gmail.com>
2024-06-08 01:45:23 +0700

    try to display something on ESP32 CAM

--- Sources/Detection_and_Classification_Comp/src/main.cpp
+++ Sources/Detection_and_Classification_Comp/src/main.cpp
@@ -5,11 +5,21 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB
+  }
+  Serial.println("Serial started at 115200");
+  
   int result = myFunction(2, 3);
+  Serial.print("myFunction(2, 3) returned: ");
+  Serial.println(result);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Hello world!");
+  delay(2000);
 }
 
 // put function definitions here:
