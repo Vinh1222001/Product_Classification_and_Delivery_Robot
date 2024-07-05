207c72484ed1d66efe11c9d88f9d7efddd39a214
Vinh1222001 <nguyentuanvinh1222@gmail.com>
2024-07-05 23:00:32 +0700

    Download some 3D models

--- Sources/Movement_and_Delivery_Comp/src/main.cpp
+++ Sources/Movement_and_Delivery_Comp/src/main.cpp
@@ -1,18 +1,47 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+#define OUT1_PIN 12
+#define OUT2_PIN 14
+#define OUT3_PIN 27
+#define OUT4_PIN 26
+#define OUT5_PIN 25
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  pinMode(OUT1_PIN, OUTPUT);
+  pinMode(OUT2_PIN, OUTPUT);
+  pinMode(OUT3_PIN, OUTPUT);
+  pinMode(OUT4_PIN, OUTPUT);
+  pinMode(OUT5_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(OUT1_PIN, HIGH);
+  digitalWrite(OUT2_PIN, LOW);
+
+  digitalWrite(OUT3_PIN, HIGH);
+  digitalWrite(OUT4_PIN, LOW);
+
+  delay(3000);
+
+  digitalWrite(OUT1_PIN, LOW);
+  digitalWrite(OUT2_PIN, HIGH);
+
+  digitalWrite(OUT3_PIN, LOW);
+  digitalWrite(OUT4_PIN, HIGH);
+
+  delay(3000);
+
+  digitalWrite(OUT1_PIN, LOW);
+  digitalWrite(OUT2_PIN, LOW);
+
+  digitalWrite(OUT3_PIN, LOW);
+  digitalWrite(OUT4_PIN, LOW);
+
+  delay(3000);
 }
